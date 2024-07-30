a51a0023e6b72328924552fddb11f5d72a186a0c
Henzo-Neves <151753624+Henzo-Neves@users.noreply.github.com>
2024-07-30 07:56:10 -0300

    .

--- .js
+++ .js
@@ -2,7 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha_texto')
 const botoes = document.querySelectorAll('.parametro-senha_botao')
 
 botoes[0].onclick = diminuirTamanho
-botoes[1].onclick = aumentarTamanhoTamanho
+botoes[1].onclick = aumentarTamanho
 
 let tamanhoSenha = 5
 numeroSenha.textContent = tamanhoSenha
