19b7abe770792e982c214948469299b5d70f283a
Henzo-Neves <151753624+Henzo-Neves@users.noreply.github.com>
2024-07-30 07:38:55 -0300

    .

--- .js
+++ .js
@@ -1,2 +1,10 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto')
+const botoes = document.querySelectorAll('.parametro-senha_botao')
+
+botoes[0].onclick = diminuirTamanho
+
+function diminuirTamanho() {
+    console.log('teste');
+}
+
 numeroSenha.textContent = 6
