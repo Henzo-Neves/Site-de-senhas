6dcf0e41d5c4e9a94ae288264f94d29d6a650d4f
Henzo-Neves <151753624+Henzo-Neves@users.noreply.github.com>
2024-07-30 07:48:41 -0300

    .

--- .js
+++ .js
@@ -3,9 +3,16 @@ const botoes = document.querySelectorAll('.parametro-senha_botao')
 
 botoes[0].onclick = diminuirTamanho
 
+let tamanhoSenha = 5
+numeroSenha.textContent = tamanhoSenha
+
 function diminuirTamanho() {
-    console.log('teste');
     tamanhoSenha = tamanhoSenha - 1
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentarTamanho() {
+    tamanhoSenha = tamanhoSenha + 1
     numeroSenha.textContent = tamanhoSenha
 }
 
