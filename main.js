6002d86fe522b0948df9660432877214a5b643bf
Henzo-Neves <151753624+Henzo-Neves@users.noreply.github.com>
2024-08-14 09:24:48 -0300

    .

--- main.js
+++ main.js
@@ -35,11 +35,11 @@ function aumentarTamanho() {
 
 function geraSenha () {
     let senha = ""
-    for(let i = 0; i < tamanhoSenha; i++)
+    for(let i = 0; i < tamanhoSenha; i++){
         let numeroAleatorio = Math.random()*maiuscula.length
         numeroAleatorio = Math.floor(numeroAleatorio)
         senha = senha + maiuscula[numeroAleatorio];
-
+    }
         console.log(senha)
+        campoSenha.value = senha
 }
-    campoSenha.value = senha
