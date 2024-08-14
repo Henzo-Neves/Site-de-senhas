26b8d8ac82964e740bfdf0aec0f05f145f45aa97
Henzo-Neves <151753624+Henzo-Neves@users.noreply.github.com>
2024-08-14 09:39:25 -0300

    .

--- main.js
+++ main.js
@@ -28,9 +28,11 @@ function diminuirTamanho(){
 }
 
 function aumentarTamanho() {
-    if(tamanhoSenha < 20)
+    if(tamanhoSenha < 20) {
     tamanhoSenha = tamanhoSenha + 1
     numeroSenha.textContent = tamanhoSenha
+    }
+    geraSenha()
 }
 
 function geraSenha () {
